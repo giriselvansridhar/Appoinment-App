40b063c13c84ced0dd0a8f4f238b25022705a3d1
Binary Bytestorm <131362593+giriselvansridhar@users.noreply.github.com>
2024-06-10 13:27:20 +0530

    1

--- appoinment-app/src/app/appointment-list/appointment-list.component.ts
+++ appoinment-app/src/app/appointment-list/appointment-list.component.ts
@@ -41,8 +41,19 @@ export class AppointmentListComponent {
         this.newAppointmentDate=new Date();
 
       }
+
+  
     
   }
+
+
+
+
+  deleteAppointment(index: number)
+{
+  this.appointments.splice(index,1);
+} 
+
 
 
 
